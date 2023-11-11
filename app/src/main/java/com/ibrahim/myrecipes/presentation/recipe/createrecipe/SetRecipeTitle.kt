320140b1fe67fd32d96867701fe078fdbe7887bf
ibrahim4851 <erogluih2001@gmail.com>
2023-11-12 00:22:27 +0300

    ui Update

--- app/src/main/java/com/ibrahim/myrecipes/presentation/recipe/createrecipe/SetRecipeTitle.kt
+++ app/src/main/java/com/ibrahim/myrecipes/presentation/recipe/createrecipe/SetRecipeTitle.kt
@@ -1,33 +1,45 @@
 package com.ibrahim.myrecipes.presentation.recipe.createrecipe
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -37,11 +49,6 @@ import com.ibrahim.myrecipes.Screen
 import com.ibrahim.myrecipes.presentation.recipe.CreateRecipeEvent
 import com.ibrahim.myrecipes.presentation.recipe.RecipeViewModel
 import com.ibrahim.myrecipes.presentation.ui.theme.Typography
-import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
-import com.maxkeppeler.sheets.duration.DurationDialog
-import com.maxkeppeler.sheets.duration.models.DurationConfig
-import com.maxkeppeler.sheets.duration.models.DurationFormat
-import com.maxkeppeler.sheets.duration.models.DurationSelection
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -52,12 +59,67 @@ fun RecipeTitle(
 
     var recipeTitle by rememberSaveable { mutableStateOf("") }
     var recipeServings by rememberSaveable { mutableStateOf("") }
-    var recipeTime by rememberSaveable { mutableStateOf("") }
+    var showTimeDialog by rememberSaveable { mutableStateOf(false) }
+    val timePickerState = rememberTimePickerState(
+        initialHour = 0,
+        initialMinute = 0,
+        is24Hour = true
+    )
+    var selectedHour by remember { mutableIntStateOf(0) }
+    var selectedMinute by remember { mutableIntStateOf(0) }
 
     Surface(
         modifier = Modifier.fillMaxSize()
     )
     {
+
+        if (showTimeDialog) {
+            AlertDialog(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(size = 12.dp)
+                    ),
+                onDismissRequest = { showTimeDialog = false }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .background(
+                            color = Color.LightGray.copy(alpha = 0.3f)
+                        )
+                        .padding(top = 28.dp, start = 20.dp, end = 20.dp, bottom = 12.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    TimePicker(state = timePickerState)
+
+                    Row(
+                        modifier = Modifier
+                            .padding(top = 12.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+
+                        TextButton(onClick = { showTimeDialog = false }) {
+                            Text(text = "Dismiss")
+                        }
+
+                        TextButton(
+                            onClick = {
+                                showTimeDialog = false
+                                selectedHour = timePickerState.hour
+                                selectedMinute = timePickerState.minute
+                            }
+                        ) {
+                            Text(text = "Confirm")
+                        }
+                    }
+                }
+            }
+        }
+
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             bottomBar = {
@@ -80,7 +142,7 @@ fun RecipeTitle(
                                 CreateRecipeEvent
                                     .SetTitleMinuteServings(
                                         recipeTitle,
-                                        recipeTime.toInt(),
+                                        selectedMinute + selectedHour,
                                         recipeServings.toInt()
                                     )
                             )
@@ -89,7 +151,7 @@ fun RecipeTitle(
                         modifier = Modifier.weight(1f),
                         enabled = recipeTitle.isNotEmpty()
                                 && recipeServings.isNotEmpty()
-                                && recipeTime.isNotEmpty()
+                                && selectedMinute + selectedHour > 0
                     ) {
                         Text(text = "Next")
                     }
@@ -165,40 +227,73 @@ fun RecipeTitle(
                     Spacer(modifier = Modifier.padding(8.dp))
 
                     Text(
-                        text = "How many minutes do we need to create this masterpiece?",
+                        text = "How long does it take to create this masterpiece?",
                         style = Typography.titleLarge
                     )
 
                     Spacer(modifier = Modifier.padding(8.dp))
 
-                    val selectedTimeInSeconds = remember { mutableLongStateOf(240) }
-                    DurationDialog(
-                        state = rememberUseCaseState(
-                            visible = true,
-                            onCloseRequest = {  }
-                        ),
-                        selection = DurationSelection { newTimeInSeconds ->
-                            selectedTimeInSeconds.longValue = newTimeInSeconds
-                        },
-                        config = DurationConfig(
-                            timeFormat = DurationFormat.HH_MM,
-                            minTime = 30,
-                            maxTime = 240
+                    Row(
+                        modifier = Modifier
+                            .width(280.dp)
+                            .height(IntrinsicSize.Max)
+                    )
+                    {
+                        Text(
+                            modifier = Modifier
+                                .border(
+                                    width = 1.dp,
+                                    color = MaterialTheme.colorScheme.secondary,
+                                    shape = RoundedCornerShape(8.dp)
+                                )
+                                .padding(5.dp),
+                            text = selectedHour.toString(),
+                            style = Typography.headlineLarge
                         )
-                    )
 
-                    OutlinedTextField(
-                        value = recipeTime,
-                        onValueChange = { value ->
-                            recipeTime = value.filter { it.isDigit() }
-                        },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                        singleLine = true,
-                        placeholder = {
-                            Text(text = "e.g. 1 hour and 15 minutes")
-                        },
-                    )
+                        Spacer(modifier = Modifier.padding(4.dp))
 
+                        Text(
+                            modifier = Modifier
+                                .height(40.dp)
+                                .align(Alignment.Bottom),
+                            text = "hours"
+                        )
+
+                        Spacer(modifier = Modifier.padding(8.dp))
+
+                        Text(
+                            modifier = Modifier
+                                .border(
+                                    width = 1.dp,
+                                    color = MaterialTheme.colorScheme.secondary,
+                                    shape = RoundedCornerShape(8.dp)
+                                )
+                                .padding(5.dp),
+                            text = selectedMinute.toString(),
+                            style = Typography.headlineLarge
+                        )
+
+                        Spacer(modifier = Modifier.padding(4.dp))
+
+                        Text(
+                            modifier = Modifier
+                                .height(40.dp)
+                                .align(Alignment.Bottom),
+                            text = "minutes"
+                        )
+                    }
+
+                    Button(
+                        modifier = Modifier
+                            .width(280.dp)
+                            .padding(top = 8.dp),
+                        onClick = {
+                            showTimeDialog = !showTimeDialog
+                        }
+                    ) {
+                        Text(text = "Set the Time")
+                    }
                 }
             }
         }
