cce8b441ea17784e1ad329956e304ef5060cd3f1
ibrahim4851 <erogluih2001@gmail.com>
2023-11-29 12:51:25 +0300

    ExpandableCard

--- app/src/main/java/com/ibrahim/myrecipes/MainActivity.kt
+++ app/src/main/java/com/ibrahim/myrecipes/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.ibrahim.myrecipes.presentation.recipedetail.ui.ExpandableCard
+import com.ibrahim.myrecipes.presentation.navigation.ScreensNavigation
 import com.ibrahim.myrecipes.presentation.ui.theme.MyRecipesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,13 +15,7 @@ class MainActivity : ComponentActivity() {
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         setContent {
             MyRecipesTheme {
-                ExpandableCard(
-                    title = "My Title",
-                    description = "Lorem ipsum dolorff sit amet, consectetur adipiscing elit, " +
-                            "sed do eiusmod tempor incididunt ut labore et dolore magna " +
-                            "aliqua. Ut enim ad minim veniam, quis nostrud exercitation " +
-                            "ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                )
+                ScreensNavigation()
             }
         }
     }
