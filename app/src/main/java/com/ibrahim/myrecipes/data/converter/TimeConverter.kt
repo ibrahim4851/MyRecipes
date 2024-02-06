f9db7cad68fb236fe45e760aa866567682555c6b
ibrahim4851 <erogluih2001@gmail.com>
2024-02-06 17:14:22 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/data/converter/TimeConverter.kt
+++ app/src/main/java/com/ibrahim/myrecipes/data/converter/TimeConverter.kt
@@ -8,9 +8,12 @@ fun Int.minutesToHourMinuteString(context: Context): String {
     val minutes = this % 60
 
     return when {
-        hours > 0 && minutes > 0 -> "$hours hrs $minutes min"
-        hours > 0 -> "$hours" + context.getString(R.string.time_converter_hours)
-        minutes > 0 -> "$minutes" + context.getString(R.string.time_converter_minutes)
+        hours > 0 && minutes > 0 -> "$hours" +
+                context.getString(R.string.time_converter_hours) +
+                " $minutes" +
+                context.getString(R.string.time_converter_minutes)
+        hours > 0 -> "$hours " + context.getString(R.string.time_converter_hours)
+        minutes > 0 -> "$minutes " + context.getString(R.string.time_converter_minutes)
         else -> "0 min"
     }
 }
