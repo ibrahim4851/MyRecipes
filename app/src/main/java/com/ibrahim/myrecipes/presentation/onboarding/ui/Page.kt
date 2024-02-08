f9614fb261cc7168cd9cb9ef22e46432e4521388
ibrahim4851 <erogluih2001@gmail.com>
2024-02-08 11:28:26 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/presentation/onboarding/ui/Page.kt
+++ app/src/main/java/com/ibrahim/myrecipes/presentation/onboarding/ui/Page.kt
@@ -1,5 +1,6 @@
 package com.ibrahim.myrecipes.presentation.onboarding.ui
 
+import android.content.Context
 import androidx.annotation.DrawableRes
 import com.ibrahim.myrecipes.R
 
@@ -8,21 +9,22 @@ data class Page(
     val description: String,
     @DrawableRes val image: Int,
 )
-
-val pages = listOf(
-    Page(
-        title = "Welcome to MyRecipes!",
-        description = "Discover a world of flavors right at your fingertips! Get ready to dive into an endless array of recipes that will inspire your next culinary adventure.",
-        image = R.drawable.pizza1
-    ),
-    Page(
-        title = "Step-by-Step Cooking Guides",
-        description = "Follow along with detailed instructions and high-quality photos. From novice cooks to seasoned chefs, our guides make cooking simple and enjoyable.",
-        image = R.drawable.hamburgeronboarding
-    ),
-    Page(
-        title = "Find Your Next Favorite Dish",
-        description = "Easily search for recipes by ingredients, cuisine, or mood. Whether you're looking for a quick lunch or a gourmet dinner, finding the perfect recipe is just a tap away.",
-        image = R.drawable.pancakeonboarding
+fun getPages(context: Context): List<Page> {
+    return listOf(
+        Page(
+            title = context.getString(R.string.onboarding_title_1),
+            description = context.getString(R.string.onboarding_content_1),
+            image = R.drawable.pizza1
+        ),
+        Page(
+            title = context.getString(R.string.onboarding_title_2),
+            description = context.getString(R.string.onboarding_content_2),
+            image = R.drawable.hamburgeronboarding
+        ),
+        Page(
+            title = context.getString(R.string.onboarding_title_3),
+            description = context.getString(R.string.onboarding_content_3),
+            image = R.drawable.pancakeonboarding
+        )
     )
-)
+}
