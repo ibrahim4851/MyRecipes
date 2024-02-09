e3e8d1c89e68fa2499e86abf52c8e261b700ad2a
ibrahim4851 <erogluih2001@gmail.com>
2024-02-09 14:49:55 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/presentation/onboarding/ui/NewOnboardingScreen.kt
+++ app/src/main/java/com/ibrahim/myrecipes/presentation/onboarding/ui/NewOnboardingScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -22,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -61,7 +63,7 @@ fun OnBoardingScreen(
             }
         }
         HorizontalPager(state = pagerState) { index ->
-            OnBoardingPage(page = pages[index])
+            OnBoardingPage(page = pages[index], modifier = Modifier.height((LocalConfiguration.current.screenHeightDp*0.8).dp))
         }
         Spacer(modifier = Modifier.weight(1f))
         Row(
