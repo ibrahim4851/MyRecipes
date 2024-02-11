e80bcc0cd68fee5432a8dca342464136aa2efbf7
ibrahim4851 <erogluih2001@gmail.com>
2024-02-11 12:49:26 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/presentation/editrecipe/ui/EditRecipeScreen.kt
+++ app/src/main/java/com/ibrahim/myrecipes/presentation/editrecipe/ui/EditRecipeScreen.kt
@@ -1,8 +1,10 @@
 package com.ibrahim.myrecipes.presentation.editrecipe.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -12,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.ibrahim.myrecipes.presentation.editrecipe.viewmodel.EditRecipeViewModel
@@ -26,7 +29,7 @@ fun EditRecipeScreen(
     var ingredients = viewModel.state.value.ingredients
 
     var recipeTitle by remember { mutableStateOf(recipe.recipeTitle) }
-    var recipeServings by remember { mutableStateOf(recipe.recipeServings) }
+    var recipeServings by remember { mutableStateOf(recipe.recipeServings.toString()) }
     var recipeCategory by remember { mutableStateOf(recipe.foodCategory) }
     var recipeInstructions by remember { mutableStateOf(recipe.recipeInstructions) }
     var recipePhotoUri by remember { mutableStateOf(recipe.recipePhotoUri) }
@@ -42,6 +45,11 @@ fun EditRecipeScreen(
             ) {
 
                 OutlinedTextField(value = recipeTitle, onValueChange = { recipeTitle = it })
+                Spacer(Modifier.size(8.dp))
+                OutlinedTextField(
+                    value = recipeServings,
+                    onValueChange = { recipeServings = it })
+                Spacer(Modifier.size(8.dp))
 
             }
         }
