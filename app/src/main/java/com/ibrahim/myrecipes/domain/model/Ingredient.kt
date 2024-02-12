ef7f3c067b7c8b42133bde26b837bc6087d0016e
ibrahim4851 <erogluih2001@gmail.com>
2024-02-12 14:05:12 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/domain/model/Ingredient.kt
+++ app/src/main/java/com/ibrahim/myrecipes/domain/model/Ingredient.kt
@@ -1,11 +1,13 @@
 package com.ibrahim.myrecipes.domain.model
 
 import android.os.Parcelable
+import androidx.compose.runtime.Stable
 import com.ibrahim.myrecipes.data.enums.IngredientQuantityUnit
 import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 
 @Parcelize
+@Stable
 data class Ingredient(
     val ingredientId: Long,
     val ownerRecipeId: Long?,
