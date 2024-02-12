c29af95e35b57fdb81d72b079528735c6d36ac3a
ibrahim4851 <erogluih2001@gmail.com>
2024-02-12 14:05:19 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/presentation/home/ui/RecipesStaggeredGrid.kt
+++ app/src/main/java/com/ibrahim/myrecipes/presentation/home/ui/RecipesStaggeredGrid.kt
@@ -1,24 +1,22 @@
 package com.ibrahim.myrecipes.presentation.home.ui
 
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.ibrahim.myrecipes.domain.model.Recipe
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun RecipesStaggeredGrid(
     recipes: List<Recipe> = dummyRecipeList,
     onRecipeItemClick: (Recipe) -> Unit,
     onDeleteClick: (Recipe) -> Unit
-){
+) {
 
     LazyVerticalStaggeredGrid(
         columns = StaggeredGridCells.Adaptive(150.dp),
@@ -26,15 +24,13 @@ fun RecipesStaggeredGrid(
         contentPadding = PaddingValues(4.dp),
         horizontalArrangement = Arrangement.spacedBy(4.dp),
         verticalItemSpacing = 4.dp
-    ){
-        items(recipes.size) { itemIndex ->
+    ) {
+        items(recipes, key = { recipe -> recipe.recipeId }) { recipe ->
             RecipeItem(
-                recipes[itemIndex],
+                recipe,
                 onRecipeItemClick = onRecipeItemClick,
                 onDeleteClick = onDeleteClick,
-                modifier = Modifier.animateItemPlacement(animationSpec = tween(
-                    durationMillis = 600
-                ))
+                modifier = Modifier
             )
         }
     }
