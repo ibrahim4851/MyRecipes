eff9e4e04226268574489a8d2811d6a45fd2e8f2
ibrahim4851 <erogluih2001@gmail.com>
2024-02-12 14:05:02 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/domain/model/Recipe.kt
+++ app/src/main/java/com/ibrahim/myrecipes/domain/model/Recipe.kt
@@ -1,10 +1,12 @@
 package com.ibrahim.myrecipes.domain.model
 
 import android.os.Parcelable
+import androidx.compose.runtime.Stable
 import com.ibrahim.myrecipes.data.enums.FoodCategory
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Stable
 data class Recipe(
     val recipeId: Long,
     val recipeTitle: String,
