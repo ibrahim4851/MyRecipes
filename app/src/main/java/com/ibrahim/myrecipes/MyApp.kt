6c7ec36dc2b5735f09e0907e766337ce00566978
ibrahim4851 <erogluih2001@gmail.com>
2024-02-13 18:49:23 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/MyApp.kt
+++ app/src/main/java/com/ibrahim/myrecipes/MyApp.kt
@@ -1,7 +1,13 @@
 package com.ibrahim.myrecipes
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApp: Application()
+class MyApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MobileAds.initialize(this)
+    }
+}
