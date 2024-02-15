60fe5b3503ad3faffa7b9bcfc5eaa2671000faf4
ibrahim4851 <erogluih2001@gmail.com>
2024-02-15 14:31:42 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/presentation/recipedetail/ui/updatedialogs/UpdateInstructionDialog.kt
+++ app/src/main/java/com/ibrahim/myrecipes/presentation/recipedetail/ui/updatedialogs/UpdateInstructionDialog.kt
@@ -1,2 +1,71 @@
 package com.ibrahim.myrecipes.presentation.recipedetail.ui.updatedialogs
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun UpdateInstructionDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: (String) -> Unit,
+    dialogTitle: String,
+    instruction: String,
+    onDelete: () -> Unit,
+    icon: ImageVector
+) {
+    var newTitle by remember { mutableStateOf(instruction) }
+
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Example Icon")
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            OutlinedTextField(
+                value = newTitle,
+                onValueChange = { newTitle = it },
+                label = { Text(text = "New Title") })
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation(newTitle)
+                },
+                enabled = (newTitle != instruction)
+            ) {
+                Text("Confirm", color = MaterialTheme.colorScheme.onBackground)
+            }
+        },
+        dismissButton = {
+            Row {
+                TextButton(
+                    onClick = {
+                        onDismissRequest()
+                    }
+                ) {
+                    Text("Dismiss", color = MaterialTheme.colorScheme.onBackground)
+                }
+                TextButton(
+                    onClick = onDelete
+                ) {
+                    Text("Delete", color = MaterialTheme.colorScheme.onBackground)
+                }
+            }
+        }
+    )
+}
