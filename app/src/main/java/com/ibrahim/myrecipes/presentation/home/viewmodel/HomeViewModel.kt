301c6e39f192a071da2e677432d708ad3b1c8841
ibrahim4851 <erogluih2001@gmail.com>
2024-02-17 12:17:42 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/presentation/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/ibrahim/myrecipes/presentation/home/viewmodel/HomeViewModel.kt
@@ -4,11 +4,15 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ibrahim.myrecipes.data.PreferencesManager
 import com.ibrahim.myrecipes.data.enums.FoodCategory
 import com.ibrahim.myrecipes.domain.model.Recipe
 import com.ibrahim.myrecipes.domain.repository.RecipeRepository
 import com.ibrahim.myrecipes.presentation.home.ui.GridItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -17,14 +21,29 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val repository: RecipeRepository
+    private val repository: RecipeRepository,
+    private val preferencesManager: PreferencesManager
 ) : ViewModel() {
 
     private val _state = mutableStateOf(HomeScreenState())
     val state: State<HomeScreenState> = _state
 
+    private val _isDarkThemeEnabled = MutableStateFlow(false)
+    val isDarkThemeEnabled: StateFlow<Boolean> = _isDarkThemeEnabled.asStateFlow()
+
     init {
         getRecipes()
+        loadThemePreference()
+    }
+
+    private fun loadThemePreference() = viewModelScope.launch {
+        preferencesManager.darkThemeEnabled.collect { isEnabled ->
+            _isDarkThemeEnabled.value = isEnabled
+        }
+    }
+
+    fun setDarkThemeEnabled(enabled: Boolean) = viewModelScope.launch {
+        preferencesManager.setDarkThemeEnabled(enabled)
     }
 
     private fun getRecipes() = viewModelScope.launch {
