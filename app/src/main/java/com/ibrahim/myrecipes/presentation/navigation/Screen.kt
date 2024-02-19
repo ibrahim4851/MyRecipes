f2d6095d8e2fb263f0f33def00cf683cf8330319
ibrahim4851 <erogluih2001@gmail.com>
2024-02-19 11:24:43 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/presentation/navigation/Screen.kt
+++ app/src/main/java/com/ibrahim/myrecipes/presentation/navigation/Screen.kt
@@ -1,4 +1,4 @@
-package com.ibrahim.myrecipes
+package com.ibrahim.myrecipes.presentation.navigation
 
 sealed class Screen(val route: String){
     data object HomeScreen: Screen(route = "home_screen")
