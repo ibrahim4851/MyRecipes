eeaa7d6d1c75aaa93186197114420f5b36058a9b
ibrahim4851 <erogluih2001@gmail.com>
2024-02-21 22:46:40 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/presentation/home/viewmodel/HomeScreenState.kt
+++ app/src/main/java/com/ibrahim/myrecipes/presentation/home/viewmodel/HomeScreenState.kt
@@ -1,7 +1,9 @@
 package com.ibrahim.myrecipes.presentation.home.viewmodel
 
+import androidx.compose.runtime.Immutable
 import com.ibrahim.myrecipes.presentation.home.ui.GridItem
 
+@Immutable
 data class HomeScreenState(
     val gridItems: List<GridItem> = listOf(),
     val permissionNotGrantedMessage: String = ""
