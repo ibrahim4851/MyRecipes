d1018986aab255ad8c539c2a3aa03a9af0637282
ibrahim4851 <erogluih2001@gmail.com>
2024-02-21 22:46:06 +0300

    update stuff

--- app/src/main/java/com/ibrahim/myrecipes/presentation/home/ui/GridItem.kt
+++ app/src/main/java/com/ibrahim/myrecipes/presentation/home/ui/GridItem.kt
@@ -1,9 +1,11 @@
 package com.ibrahim.myrecipes.presentation.home.ui
 
+import androidx.compose.runtime.Immutable
 import com.ibrahim.myrecipes.domain.model.Recipe
 import java.util.UUID
 
+@Immutable
 sealed class GridItem {
-    data class RecipeItem(val recipe: Recipe) : GridItem()
-    data class AdItem(val adId: UUID) : GridItem()
+    @Immutable data class RecipeItem(val recipe: Recipe) : GridItem()
+    @Immutable data class AdItem(val adId: UUID) : GridItem()
 }
